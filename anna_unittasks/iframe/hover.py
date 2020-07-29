61732056e053390f0504b32b3dff74de02ced4e6
Patrik Pihlstrom <patrik.pihlstrom@gmail.com>
2020-07-29 20:48:40 +0200

    spaces -> tabs

--- anna_unittasks/iframe/hover.py
+++ anna_unittasks/iframe/hover.py
@@ -2,8 +2,8 @@ from anna_lib.task.abstract_task import AbstractTask
 
 
 class Hover(AbstractTask):
-    def before_execute(self):
-        self.timeout = 1
+	def before_execute(self):
+		self.timeout = 1
 
 	def __execute__(self):
 		self.scroll_to('#iframe-test-hover')
